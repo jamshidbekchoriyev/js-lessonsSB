0ed7626ca57674a19085c6fd1310a7e43fa2e776
Jamshid <example@gmail.com>
2023-12-03 17:38:25 +0500

    salom

--- script.js
+++ script.js
@@ -1,27 +1,66 @@
 window.onload = function(){
     "use strict";
 
-    const numberOfSeries = +prompt("Nechta serial ko'rdingiz?", "");
+    // const numberOfSeries = +prompt("Nechta serial ko'rdingiz?", "");
 
-    const seriesDB = {
-        count: numberOfSeries,
-        series: {},
-        actors: {},
-        genres: [],
-        private: false,
-    };
+    // const seriesDB = {
+    //     count: numberOfSeries,
+    //     series: {},
+    //     actors: {},
+    //     genres: [],
+    //     private: false,
+    // };
 
-    const a = prompt("Oxirgi ko'rgan serialingiz?"),
-    b = prompt ("Nechi baho bergan bo'lardingiz?"),
-    c = prompt("Oxirgi ko'rgan serialingiz?"),
-    d = prompt ("Nechi baho bergan bo'lardingiz?");
+    // const a = prompt("Oxirgi ko'rgan serialingiz?"),
+    // b = prompt ("Nechi baho bergan bo'lardingiz?"),
+    // c = prompt("Oxirgi ko'rgan serialingiz?"),
+    // d = prompt ("Nechi baho bergan bo'lardingiz?");
 
-    seriesDB.series[a] = b;
-    seriesDB.series[c] = d;
+    // seriesDB.series[a] = b;
+    // seriesDB.series[c] = d;
 
-    console.log(seriesDB)
+    // console.log(seriesDB)
 
 
     
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const numberOfSeries = +prompt("Nechta serial ko'rdingiz?");
+
+const seriesDb = {
+    count: numberOfSeries,
+    series: {},
+    actors: {},
+    genres: [],
+    private: false,
 }
+
+const a = prompt("Oxirgi ko'rgan serialingiz?"),
+    b = prompt("Nechi baho berasz?"),
+    c = prompt("Oxirgi ko'rgan serialingiz?")
+    d = prompt("Nechi baho berasz?");
+
+}
+
+seriesDb.series[a]= b;
+seriesDb.series[c]= d;
+
+console.log(seriesDb)
